9b347b2cde2e0f07ba64366d4684bf26195da284
chethack <techbuddy022@gmail.com>
2023-12-15 19:47:20 +0530

    function with return and various types of function syntax

--- Function/src/main/kotlin/Function.kt
+++ Function/src/main/kotlin/Function.kt
@@ -6,16 +6,29 @@ fun main () {
     val base = readln().toInt()
     print("To exponent value to calculate: ")
     val exponent = readln().toInt()
-    printPower(base, exponent)
+    val value = printPower(base, exponent)
+    println("$base to the power of $exponent is: $value")
+    val product = multiply(base, exponent)
+    println("Product of $base and $exponent is $product")
+    val list = listOf(1, 2, 3, 4)
+    val index = 2
+    println("Value at index $index in list $list is ${valueAt(list, index)}")
+
 }
 fun doSomething(doingThis: String) {
     println("Yo! I just did $doingThis")
 }
 
-fun printPower(base: Int, exponent: Int) {
+fun printPower(base: Int, exponent: Int): Int {
     var result = 1
     for(i in 1..exponent) {
         result *= base
     }
-    println("$base to the power of $exponent is: $result")
+    return result
 }
+
+//Single line function
+fun multiply(a: Int, b: Int) = a * b
+
+//function that takes in list input
+fun valueAt(list: List<Int>, index: Int) = list[index]
